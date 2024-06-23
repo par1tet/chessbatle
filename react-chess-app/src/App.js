74caf88b53a4ef67b935ba0e7b993e7902f5166c
par1tet <mankova486@gmail.com>
2024-06-23 18:29:54 +0300

    kill me блять

--- react-chess-app/src/App.js
+++ react-chess-app/src/App.js
@@ -1,21 +1,25 @@
 import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 import Piece from './components/Piece.jsx';
+import io, { Socket } from 'socket.io-client'
 import './App.css'
+
+const socket = io.connect("http://192.168.0.110:8000")
 
 function App() {
 	const [field, setField] = useState([])
 	const [coordsOut, setCoordsOut] = useState([])
+	const [isSelected, setIsSelected] = useState(false)
 
 	useEffect(() => {
-		axios.get('http://localhost:8000/get_field')
+		axios.get('http://192.168.0.110:8000/get_field')
 		.then(r => {
 			setField(r.data.field)
 		})
 	}, [])
 
 	function updateField(){
-		axios.get('http://localhost:8000/get_field')
+		axios.get('http://192.168.0.110:8000/get_field')
 		.then(r => {
 			setField(r.data.field)
 		})
@@ -44,36 +48,43 @@ function App() {
 
 	function cellBeenClick(e){
 		e.stopPropagation()
-		if (coordsOut[0] !== undefined){
+		console.log(e.target.attributes.celldata.value);
+		if (isSelected){
 			let numberOfCell = e.target.attributes[1].value
 			let documentBoard = document.querySelector('.board')
 			let cells = documentBoard.children
+			console.log(23)
 
-			axios.post('http://localhost:8000/can_to_move',{
+
+			axios.post('http://192.168.0.110:8000/can_to_move',{
 				"coordsOut": coordsOut,
 				"coordsIn": [
 					Math.trunc(numberOfCell / 8),
 					numberOfCell % 8
 				],
-				// "piece": cells[(coordsOut[0]*8+coordsOut[1])].innerHTML
+				"piece": cells[(coordsOut[0]*8+coordsOut[1])].attributes.celldata.value[0]
 			})
 			.then(r => {
-				// console.log(r.data)
+				console.log(r.data)
 				if(r.data.toCanMove === true){
+					setIsSelected(false)
 					setCoordsOut([])
+					for (let i = 0;i < cells.length;i++){
+						cells[i].classList.remove('selected')
+					}
 				}
 				updateField()
 			})
-
-			for (let i = 0;i < cells.length;i++){
-				cells[i].classList.remove('selected')
-			}
 		}else{
 			if(e.target.attributes.celldata.value === '  '){
+				console.log(34)
 				return 0;
 			}
 			e.target.classList.add('selected')
 			let numberOfCell = e.target.attributes[1].value
+			console.log(234)
+
+			setIsSelected(true)
 
 			setCoordsOut([
 				Math.trunc(numberOfCell / 8),
@@ -89,16 +100,14 @@ function App() {
 			<div className='board'>
 				{field.map((row, indexRow) =>
 					row.map((cell, indexColumn) =>
-						<button
+						<Piece
 							className={'cell-board'}
 							number={indexRow*8+indexColumn}
 							key={indexRow*8+indexColumn}
 							onClick={e => cellBeenClick(e)}
 							celldata={cell}
 						>
-							<Piece cellName={cell} number={indexRow*8+indexColumn} celldata={cell}>
-							</Piece>
-						</button>
+						</Piece>
 					)
 				)}
 			</div>
--- react-chess-app/src/components/Piece.jsx
+++ react-chess-app/src/components/Piece.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
 
 function Piece(props) {
-	function getPhoto(cell){
-		let src = ''
-
-		if (cell === '  '){
-			src = ''
-		}else if(cell === 'Rw'){
-			src = 'https://images.vexels.com/media/users/3/254572/isolated/preview/1a4650d1e776db1b0f5c0eef84642db4-simple-rook-chess-piece-cut-out.png?width=1130'
-		}
-
-		return src
-	}
-
     function getClass(cell){
 		let currentClass = ''
 
@@ -25,7 +13,12 @@ function Piece(props) {
 		return currentClass
 	}
 
-    return ( <img className={getClass(props.cellName)} number={props.number} celldata={props.celldata} src={getPhoto(props.cellName)}></img> );
+    return (<div
+				className={'cell-board piece' + getClass(props.cellName)}
+				onClick={e => {props.onClick(e)}}
+				number={props.number}
+				celldata={props.celldata}
+			></div> );
 }
 
 export default Piece;
